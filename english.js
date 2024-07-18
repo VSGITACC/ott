35ac0b804c2ec045acf0b2ce7e62c95a364d5642
Vedsai Abbaraju <vedsai.abbaraju@gspann.com>
2024-07-18 15:06:18 +0530

    redo changes 404

--- english.js
+++ english.js
@@ -1,9 +1,20 @@
 var slideIndex = 1;
 showDivs(slideIndex);
+var slidIndex = 1;
+showDiv(slidIndex);
+var sliIndex = 1;
+showDi(sliIndex);
 
 function plusDivs(n) {
   showDivs(slideIndex += n);
 }
+function plusDivs(n) {
+    showDiv(slidIndex += n);
+  }
+
+  function plusDivs(n) {
+    showDi(sliIndex += n);
+  }
 
 function showDivs(n) {
   var i;
@@ -15,3 +26,25 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
 }
+
+function showDiv(n) {
+    var i;
+    var x = document.getElementsByClassName("mSlides");
+    if (n > x.length) {slidIndex = 1}
+    if (n < 1) {slidIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    x[slidIndex-1].style.display = "block";  
+  }
+
+  function showDi(n) {
+    var i;
+    var x = document.getElementsByClassName("mylides");
+    if (n > x.length) {sliIndex = 1}
+    if (n < 1) {sliIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    x[sliIndex-1].style.display = "block";  
+  }
